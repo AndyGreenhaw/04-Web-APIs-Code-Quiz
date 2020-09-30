2b5e6bf0f38e19dd6c4926348b68a51f4427621d
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-09-30 09:30:41 -0600

    uggggggggh

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ var body = document.body;
 var h1El = document.createElement("h1");
 var h2El = document.createElement("h2");
 var divTag = document.createElement("div");
-var buttonEl = document.createElement("button");
+var startButton = document.createElement("button");
 var bTag = document.createElement("p");
 var correctText = document.createElement("h3");
 var incorrectText = document.createElement("h3");
+var qTag = document.createElement("p");
+   
 var score = 0;
 var count = 60;
 
@@ -22,43 +24,24 @@ h1El.textContent = "Welcome to my quiz!";
 h2El.textContent = "Push the Start Button to start the quiz, and try to answer the questions within the time limit. Keep in mind, any question you answer wrong will penalize your score time by ten seconds.";
 correctText.textContent = "CORRECT!"
 incorrectText.textContent = "INCORRECT!"
-buttonEl.textContent = "Start Quiz";
+startButton.textContent = "Start Quiz";
 
 // Stylize Elements //
 h1El.setAttribute("style", "font-family: arial; font-size: 48px; color:blue; margin-top: 20px; margin-bottom: 20px; text-align: center;");
 h2El.setAttribute("style", "font-family: arial; font-size: 24px; margin-top: 20px; margin-bottom: 20px; text-align:center;");
-buttonEl.setAttribute("style", "text-align: center; color:white; background-color:blue; width: 100px;");
+startButton.setAttribute("style", "text-align: center; color:white; background-color:blue; width: 100px;");
 bTag.setAttribute("style", "text-align: center;");
 correctText.setAttribute("style", "font-family: arial; font-size: 32px; color:blue; margin-top: 30px; margin-bottom: 20px; text-align: center;");
 incorrectText.setAttribute("style", "font-family: arial; font-size: 32px; color:blue; margin-top: 30px; margin-bottom: 20px; text-align: center;");
+qTag.setAttribute("style", "text-align: center;");
 
 // Append Starter Elements //
 body.appendChild(h1El);
 body.appendChild(h2El);
 body.appendChild(bTag);
-bTag.appendChild(buttonEl);
+bTag.appendChild(startButton);
 
-// Correct Function //
-function correctAnswer(){
-    body.appendChild(correctText);
-    score = (score + 1);
-};
-
-// Incorrect Function //
-function incorrectAnswer(){
-    body.appendChild(incorrectText);
-    count = (count-10);  
-};
-
-// Questions //
-
-var questionArray = [question1, question2, question3, question4, question5];
-
-////////////////
-// QUIZ START //
-////////////////
-
-// Question 1 //
+// Question Objects //
 var question1 = {
     headline : "Question 1",
     question : "How many legs does a dog have?",
@@ -71,88 +54,208 @@ var question1 = {
     correctAnswer : "Four",
 };
 
-var test = question1.correctAnswer;
-console.log(test);
+var question2 = {
 
-buttonEl.addEventListener("click", firstQuestion);
+    question : "Cats or dogs?",
+    correctAnswer : "correct",
+    incorrectAnswers : [
+        "incorrect1",
+        "incorrect2",
+        "incorrect3",
+        "incorrect4"
+    ]
+};
+
+var question3 = {
+
+    question : "Cats or dogs?",
+    correctAnswer : "correct",
+    incorrectAnswers : [
+        "answer1",
+        "answer2",
+        "answer3",
+        "answer4"
+    ]
+};
+
+var question4 = {
+
+    question : "Cats or dogs?",
+    correctAnswer : "correct",
+    incorrectAnswers : [
+        "answer1",
+        "answer2",
+        "answer3",
+        "answer4"
+    ]
+};
+
+var question5 = {
+
+    question : "Cats or dogs?",
+    correctAnswer : "correct",
+    incorrectAnswers : [
+        "answer1",
+        "answer2",
+        "answer3",
+        "answer4"
+    ]
+};
+
+// Question Object Array //
+var index = 0;
+var questionArray = [question1, question2, question3, question4, question5];
+
+////////////////
+// QUIZ START //
+////////////////
+
+
+
+startButton.addEventListener("click", questionsFunction);
 
 //////////////////////////////////////////////////////////
 //FIRST QUESTION: Activates after clicking Start Button //
 //////////////////////////////////////////////////////////
-function firstQuestion(e){
+function questionsFunction(e){
     e.preventDefault();
 
     //Remove Start Button//
-    buttonEl.setAttribute("style", "display:none;");
+    startButton.setAttribute("style", "display:none;");
 
-    //Add Headline and Question//
-    h1El.textContent = question1.headline;
-    h2El.textContent = question1.question;
+    for(var i = 0; i < questionArray.length ; i++){
 
-    //Build Loop to Set Up Possible Answers//
-    for(var i = 0; i < question1.possibleAnswers.length; i++){
-    //question1.possibleAnswers.forEach(function(choice){
-        //console.log(choice);
+        //Add Headline and Question//
+        h1El.textContent = questionArray[i].headline;
+        h2El.textContent = questionArray[i].question;
+        console.log(question1.possibleAnswers.length);
 
-        //Set Up P Tags for Double Spacing
-        var qTag = document.createElement("p");
-            qTag.setAttribute("style", "text-align: center;");
-        
-        //Set Up Choice Buttons 
-        var answers1Button = document.createElement("button");
-            body.appendChild(qTag);
-            //Pulls Text from Objects to Buttons
-            answers1Button.textContent = question1.possibleAnswers[i];
-            //Stylizes Buttons
-            answers1Button.setAttribute("style", "text-align: center; color:white; background-color:blue; width: 100px;");
-            answers1Button.setAttribute("id", "choice" + (i+1));
-            answers1Button.setAttribute("class", "buttons");
-            answers1Button.setAttribute("value", (i+1));
-            //Place Buttons in P Tags
-            qTag.appendChild(answers1Button);
-    };
+        //Build Loop to Set Up Possible Answers//
+        for(var q=0; q < questionArray[i].length; q++) {
 
-    var correct = document.getElementById("choice4");
-    var incorrect = ???????
-    
-    console.log(incorrect);
+            //Set Up P Tags (qTags) for Double Spacing
+            var qTag = document.createElement("p");
+                qTag.setAttribute("style", "text-align: center;");
+            
+            //Set Up Choice Buttons 
+            var answers1Button = document.createElement("button");
+                body.appendChild(qTag);
+                
+                //Pulls Text from Objects to Buttons
+                answers1Button.textContent = questionArray[i].possibleAnswers[q];
+                
+                //Stylizes Buttons
+                answers1Button.setAttribute("style", "text-align: center; color:white; background-color:blue; width: 100px;");
+                answers1Button.setAttribute("id", "choice" + (q+1));
+                answers1Button.setAttribute("class", "buttons");
+                answers1Button.setAttribute("value", (q+1));
+                
+                //Place Buttons in P Tags
+                qTag.appendChild(answers1Button);
 
-    correct.onclick = function(e){
-        body.appendChild(correctText);
-        score = (score + 1);
-        console.log(score);
+                var correct;
+                var incorrect;
+            
+                // User Chooses Button //
+                answers1Button.onclick = function(e){
+                    e.preventDefault();
+                    console.log(this.value);
 
-        secondQuestion();
-    };
+                    // If Correct, Add 1 to Score and Move to Question 2 Function //
+                    if (this.value == '4') {
+                        body.appendChild(correctText);
+                        score = (score + 1);
+                        console.log(score);
 
-    incorrect.onclick = function(e){
-        body.appendChild(incorrectText);
-        score = (score + 1);
-        console.log(score);
+                        secondQuestion();
 
-        secondQuestion();
+                    // If Incorrect, Add 1 to Score and Move to Question 2 Function //
+                    } else {
+                        body.appendChild(incorrectText);
+                        count = (count - 10);
+                        console.log(score);
+                
+                        secondQuestion();
+                    };
+                };
+        };
     };
 };
 
+/*
+    function secondQuestion(e){
+    e.preventDefault();
+    console.log("Is this working?")
+    
+        //Clear Old Content
+        h1El.setAttribute("style", "display:none;");
+        h2El.setAttribute("style", "display:none;");
+        qTag.setAttribute("style", "display:none;");
+        answers1button.setAttribute("style", "display:none;");
+
+        //Add Headline and Question//
+        h1El.textContent = question1.headline;
+        h2El.textContent = question1.question;
+        console.log(question1.possibleAnswers.length);
+
+        //Build Loop to Set Up Possible Answers//
+        for(var i = 0; i < question1.possibleAnswers.length; i++){
+        
+        //question1.possibleAnswers.forEach(function(choice){
+
+            //Set Up P Tags (qTags) for Double Spacing
+            var qTag = document.createElement("p");
+                qTag.setAttribute("style", "text-align: center;");
+            
+            //Set Up Choice Buttons 
+            var answers1Button = document.createElement("button");
+                body.appendChild(qTag);
+
+                //Pulls Text from Objects to Buttons
+                answers1Button.textContent = question1.possibleAnswers[i];
+
+                //Stylizes Buttons
+                answers1Button.setAttribute("style", "text-align: center; color:white; background-color:blue; width: 100px;");
+                answers1Button.setAttribute("id", "choice" + (i+1));
+                answers1Button.setAttribute("class", "buttons");
+                answers1Button.setAttribute("value", (i+1));
+                //Place Buttons in P Tags
+                qTag.appendChild(answers1Button);
+            
+                // User Chooses Button //
+                answers1Button.onclick = function(e){
+                    e.preventDefault();
+                    console.log(this.value);
+                
+                // If Correct, Add 1 to Score and Move to Question 2 Function //
+                if (this.value == '4') {
+                    body.appendChild(correctText);
+                    score = (score + 1);
+                    console.log(score);
+
+                    secondQuestion();
+            
+                // If Incorrect, Add 1 to Score and Move to Question 2 Function //
+                } else {
+                    body.appendChild(incorrectText);
+                    score = (score + 1);
+                    console.log(score);
+            
+                    secondQuestion();
+                };
+            };
+        };
+    };
+};
+*/
 
 //////////////////////////////////////////////////////////
 //SECOND QUESTION: Activates after clicking Start Button //
-//////////////////////////////////////////////////////////
-// Question 2 //
-var question2 = {
-    headline : "Question 2",
-    question : "How many legs does a human have?",
-    possibleAnswers : [
-        "One",
-        "Two",
-        "Three",
-        "Four"
-    ],
-    correctAnswer : "Four",
-};
+/////////////////////////////////////////////////////////
 
+/* DEAD ZONE
 function secondQuestion(e){
-    e.preventDefault();
+    //e.preventDefault();
 
 
 //Remove Buttons//
@@ -245,56 +348,7 @@ h2El.textContent = question2.question;
 // CREATE AN OBJECT WITH AN ARRAY OF HIGH SCORES
 
 //var allQuestions = [question1, question2, question3, question4, question5]
+*/ 
 
-var question2 = {
-
-    question : "Cats or dogs?",
-    correctAnswer : "correct",
-    incorrectAnswers : [
-        "incorrect1",
-        "incorrect2",
-        "incorrect3",
-        "incorrect4"
-    ]
-};
-
-var question3 = {
-
-    question : "Cats or dogs?",
-    correctAnswer : "correct",
-    incorrectAnswers : [
-        "answer1",
-        "answer2",
-        "answer3",
-        "answer4"
-    ]
-
-};
-
-var question4 = {
-
-    question : "Cats or dogs?",
-    correctAnswer : "correct",
-    incorrectAnswers : [
-        "answer1",
-        "answer2",
-        "answer3",
-        "answer4"
-    ]
-
-};
-
-var question5 = {
-
-    question : "Cats or dogs?",
-    correctAnswer : "correct",
-    incorrectAnswers : [
-        "answer1",
-        "answer2",
-        "answer3",
-        "answer4"
-    ]
-
-};
 
 
